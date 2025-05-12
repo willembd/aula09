722b39808f5d54219c4df653f375f1674402ccc0
Willemberg Bruno <willem.bruno@hotmail.com>
2025-05-12 10:28:16 -0300

    atualizando exercicio10

--- exercicio10.py
+++ exercicio10.py
@@ -1,1 +1,8 @@
-print("fazer em casa")
+cont=0
+frase = input("Digite uma frase: ")
+
+for i in range(len(frase)):
+    if frase[i] == "a":
+        cont+=1
+
+print(f"{cont} letras (a) tem na frase")
